6e91891af44cb27d20543545d09ab0018b3ff9a3
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-08-12 13:27:35 -0300

    corrigindo link home do blog para site 2

--- blog/bl-themes/nimbus/php/navigation.php
+++ blog/bl-themes/nimbus/php/navigation.php
@@ -3,6 +3,7 @@
     <?php foreach ($categories->db as $key=>$fields):
     if($fields['list']):  ?>
     <li>
+      <a href="https://lightgrey-manatee-733013.hostingersite.com/">Site</a>
       <a href="<?php echo DOMAIN_CATEGORIES.$key; ?>"><?php echo $fields['name']; ?></a>
     </li>
     <?php endif;endforeach; ?>
@@ -10,4 +11,3 @@
   <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
 </nav>
 <a class="btn-getstarted" href="<?php echo $site->url(); ?>">Início</a>
-<a class="btn-getstarted" href="https://lightgrey-manatee-733013.hostingersite.com/">Site</a>
