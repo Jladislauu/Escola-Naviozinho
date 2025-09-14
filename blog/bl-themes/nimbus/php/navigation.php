9b761f4ce3d08a1451badc94faf1fceb429240f9
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-09-14 19:47:55 -0300

    atualizando link do blog para o site

--- blog/bl-themes/nimbus/php/navigation.php
+++ blog/bl-themes/nimbus/php/navigation.php
@@ -10,4 +10,4 @@
   <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
 </nav>
 <a class="btn-getstarted" href="<?php echo $site->url(); ?>">Início</a>
-<a class="btn-getstarted" href="https://lightgrey-manatee-733013.hostingersite.com/">Site</a>
+<a class="btn-getstarted" href="https://naviozinho.com/">Site</a>
